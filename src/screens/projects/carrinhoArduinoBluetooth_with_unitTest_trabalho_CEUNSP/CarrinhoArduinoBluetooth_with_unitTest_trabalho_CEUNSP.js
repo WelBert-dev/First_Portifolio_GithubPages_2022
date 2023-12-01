4c29806c58cf8422b8890f125234261703c32ef0
WelBert-dev <wellison.bertelli@hotmail.com>
2023-12-01 13:54:19 -0300

    [Projects Screen]: Carrinho arduíno bluetooth: Seção ArduinoUnit e Polimorfismo com explicações sobre o redirecionamento das saídas dos testes unitários, enviando para o aplicativo android Dabble.
    - Isso remove a necessidade de manter o arduíno conectado no computador via USB para monitoramento nos testes unitários.
    
    - Assim, podemos disparar e visualizar esses feedbacks, tudo pelo mesmo ambiente: Terminal Module do Aplicativo android Dabble.

--- src/screens/projects/carrinhoArduinoBluetooth_with_unitTest_trabalho_CEUNSP/CarrinhoArduinoBluetooth_with_unitTest_trabalho_CEUNSP.js
+++ src/screens/projects/carrinhoArduinoBluetooth_with_unitTest_trabalho_CEUNSP/CarrinhoArduinoBluetooth_with_unitTest_trabalho_CEUNSP.js
@@ -135,6 +135,100 @@ export default function CarrinhoArduinoBluetooth_with_unitTest_trabalho_CEUNSP()
                                 </ul>
                             </li>   
                         </ul>
+                    </li>
+
+                    <li>
+                        <h3 class="projects-subtitle">ArduinoUnit e Polimorfismo:</h3>
+                        <ul className="projects-frontend--container">
+                            <li>A saída padrão dos resultados dos testes para monitoramentos e feedbacks sobre eles, é por meio do stdout <code className="-efeitoMarcaTexto">Serial.print...()</code> nativo do arduíno.</li>
+                            <li>Ou seja, para debbugar e verificar esses resultados, deve-se estar com o arduíno conectado no computador via cabo USB.</li>
+                            <li>E visualizar por meio da janela serial do ArduínoIDE.</li>
+                            <li>Porém isso é bem chato e desgastante, então resolvi me desafiar ainda mais.</li>
+                            <li>Estamos com uma conexão Bluetooth estabelecida entre o celular e o carrinho.</li>
+                            <li>Então podemos utilizar o mesmo ambiente como interface de comunicação homen-máquina, para monitoramento dos testes: Aplicativo Android Dabble - Terminal Module.</li>
+                            <li>Com uso do polimorfismo, podemos implementar uma classe compatível com <code className="-efeitoMarcaTexto">Print</code> herdando dele.</li>
+                            <li>Pois "por debaixo dos panos" a biblioteca ArduinoUnit utiliza os métodos dele para realizar impressões print, uma vez que ele é a super classe do <code className="-efeitoMarcaTexto">Serial</code></li>
+                            <li className="-marginNone--inMobile"><p className="-listItem--inMobile --newThemeBWMCiano">Então basta sobrescrever o método <code className="-efeitoMarcaTexto">write()</code> do <code className="-efeitoMarcaTexto">Print</code>, pois ele é executado por todos os métodos de impressões do <code className="-efeitoMarcaTexto">Serial.print...()</code></p>
+                                <p className="main-title--implementFullBlock">Exemplo de classe compatível com Print utilizando polimorfismo por meio de herança:</p>
+                                <ul className="main-implementFullBlock--container">
+                                <code className="implementFullBlock">    
+
+                                    <code className="-tokenKeyword">class</code> <code className="-tokenClassEntity">PrintAdapterArduinoUnitToTerminalDabble</code> : <code className="-tokenKeyword">public</code> <code className="-tokenClassEntity">Print</code> &#123;<br/>
+                                    <code className="-tokenKeyword">public:</code><br/>
+                                        <code className="-nestedInnerCode"><code className="-tokenClassEntity">String</code> buffer;</code><br/>
+
+                                        <br/>
+                                        <code className="-nestedInnerCode"><code className="-tokenKeyConstant">size_t</code> <code className="-tokenMethod">write</code>(<code className="-tokenKeyConstant">uint8_t</code> c) <code className="-tokenKeyword">override</code> &#123;</code><br/>
+                                        <code className="-nestedInnerCode --2Identation">buffer += (<code className="-tokenKeyword">char</code>) c;</code><br/>
+                                        <code className="-nestedInnerCode --2Identation"><code className="-tokenKeyword">return</code> <code className="-tokenKeyConstant">1</code>;</code><br/>
+                                        <code className="-nestedInnerCode">&#125;</code><br/>
+
+                                        <br/>
+                                        <code className="-nestedInnerCode"><code className="-tokenKeyword">void</code> <code className="-tokenMethod">sendMsgToTerminalDabble</code>() &#123;</code><br/>
+                                            <code className="-nestedInnerCode --2Identation"><code className="-tokenClassEntity">Terminal</code>.<code className="-tokenMethod">print</code>(buffer);</code><br/>
+                                            <code className="-nestedInnerCode --2Identation">buffer = <code className="-tokenString">""</code>; <span className="-tokenComment">// Limpa o buffer após o envio.</span></code><br/>
+                                        <code className="-nestedInnerCode">&#125;</code><br/>
+
+                                        <br/>
+                                        <code className="-nestedInnerCode"><code className="-tokenKeyword">void</code> <code className="-tokenMethod">sendMsgToTerminalDabble</code>(<code className="-tokenClassEntity">String</code> message) &#123;</code><br/>
+                                            <code className="-nestedInnerCode --2Identation"><code className="-tokenClassEntity">Terminal</code>.<code className="-tokenMethod">println</code>(message);</code><br/>
+                                        <code className="-nestedInnerCode">&#125;</code><br/>
+    
+                                    &#125;<br/>
+
+                                    <br/>
+                                    <code className="-tokenClassEntity">PrintAdapterArduinoUnitToTerminalDabble</code> myPrintAdapter_arduinoUnitStdout_toTerminalDabbleBluetooth;     
+                                </code>                                    
+                                </ul>
+                            </li>  
+                            <li className="-marginNone--inMobile"><p className="-listItem--inMobile --newThemeBWMCiano">E depois, alterar o ponteiro padrão do <code className="-efeitoMarcaTexto">Test::out = &Serial;</code> para uma instância da classe compatível com <code className="-efeitoMarcaTexto">Print</code> criado por nós anteriormente.</p>
+                                <p className="main-title--implementFullBlock">Exemplo de como alterar o ponteiro, não precisa ser necessáriamente neste bloco, mas deve ser executado antes da bateria de testes unitários:</p>
+                                <ul className="main-implementFullBlock--container">
+                                <code className="implementFullBlock">    
+
+                                    <code className="-tokenKeyword">void</code> <code className="-tokenMethod">setup</code>() &#123;<br/>
+                                        <code className="-nestedInnerCode">...</code><br/>
+                                        <code className="-nestedInnerCode"><code className="-tokenClassEntity">Test</code>::<code className="-tokenMethod">out</code> =  &myPrintAdapter_arduinoUnitStdout_toTerminalDabbleBluetooth;</code><br/>
+                                        <code className="-nestedInnerCode">...</code><br/>
+                                    &#125;
+                                </code>                                    
+                                </ul>
+                            </li>  
+
+                            <li>Desta forma, podemos personalizar as regras de saída serial, redirecionando todas as impressões para outros módulos do projeto.</li>
+                            <li>Neste caso, redirecionamos para o Terminal Module, que é outra funcionalidade do aplicativo android Dabble.</li>
+
+                            <li className="-marginNone--inMobile"><p className="-listItem--inMobile --newThemeBWMCiano">Executamos essa ação após finalizar toda a bateria de testes unitários, enviando o buffer acumulado.</p>
+                                <p className="main-title--implementFullBlock">Exemplo de chamada para execução da bateria de testes, e após finalizado, envia o feedback dos testes acumulados no buffer:</p>
+                                <ul className="main-implementFullBlock--container">
+                                <code className="implementFullBlock">    
+                                    <code className="-tokenKeyword">int</code> count = <code className="-tokenKeyConstant">0</code>;<br/>
+                                    <code className="-tokenKeyword">int</code> qtde_execution_all_tests_cases = <code className="-tokenKeyConstant">1</code>;<br/>
+
+                                    <br/>
+                                    <code className="-tokenKeyword">void</code> <code className="-tokenMethod">run_all_unit_test</code>() &#123;<br/>
+
+                                        <code className="-nestedInnerCode"><code className="-tokenKeyword">while</code> (count &#62;= qtde_execution_all_tests_cases) &#123;</code><br/>
+
+                                        <code className="-nestedInnerCode --2Identation"><code className="-tokenMethod">sendMsgToTerminalDabble</code>(<code className="-tokenString">"Teste Número: "</code>+<code className="-tokenClassEntity">String</code>(count+<code className="-tokenKeyConstant">1</code>));</code><br/> 
+                                        <code className="-nestedInnerCode --2Identation"><code className="-tokenMethod">sendMsgToTerminalDabble</code>(<code className="-tokenString">"Iniciando bateria de testes..."</code>);</code><br/> 
+                                        <code className="-nestedInnerCode --2Identation"><code className="-tokenClassEntity">Test</code>::<code className="-tokenMethod">run</code>();</code><br/>
+                                        <code className="-nestedInnerCode --2Identation"><code className="-tokenMethod">sendMsgToTerminalDabble</code>(<code className="-tokenString">"Finalizando bateria de testes..."</code>);</code><br/> 
+                                        <code className="-nestedInnerCode --2Identation">count++;</code><br/>
+                                        <code className="-nestedInnerCode">&#125;</code><br/>
+
+                                        <br/>
+                                        <code className="-nestedInnerCode"><code className="-tokenMethod">sendMsgToTerminalDabble</code>(<code className="-tokenString">"Generating Summary..."</code>);</code><br/> 
+
+                                        <code className="-nestedInnerCode"><span className="-tokenComment">// Após finalizado, envia buffer acumulado do `Test::out.print()` para Terminal Module:</span></code><br/>
+                                        <code className="-nestedInnerCode"><code className="-tokenMethod">sendMsgToTerminalDabble</code>();</code><br/> 
+
+                                        <code className="-nestedInnerCode">count = <code className="-tokenKeyConstant">0</code>;</code><br/>
+                                    &#125;
+                                </code>                                    
+                                </ul>
+                            </li>  
+                        </ul>
                     </li>      
                 </ul>
             </div>
