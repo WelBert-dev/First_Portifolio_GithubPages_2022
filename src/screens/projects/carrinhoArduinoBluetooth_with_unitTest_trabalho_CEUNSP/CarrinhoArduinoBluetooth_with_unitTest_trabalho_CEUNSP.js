b23bb4e36fa2ac01f83ca17e6b7ab3b099171cd2
WelBert-dev <wellison.bertelli@hotmail.com>
2023-12-01 13:36:01 -0300

    [Projects Screen]: Carrinho arduíno bluetooth: Seção Lib ArduinoUnit com explicações sobre a biblioteca do arduíno especializada em testes unitários (bem similar ao JUnit do Java), por meio dos Assertions.

--- src/screens/projects/carrinhoArduinoBluetooth_with_unitTest_trabalho_CEUNSP/CarrinhoArduinoBluetooth_with_unitTest_trabalho_CEUNSP.js
+++ src/screens/projects/carrinhoArduinoBluetooth_with_unitTest_trabalho_CEUNSP/CarrinhoArduinoBluetooth_with_unitTest_trabalho_CEUNSP.js
@@ -52,6 +52,32 @@ export default function CarrinhoArduinoBluetooth_with_unitTest_trabalho_CEUNSP()
                             <li>Direções estavam trocadas, a funcionalidade "para frente" indo "para esquerda", e para os lados também incorretos.</li>
                             <li>SOLUÇÃO: Bastanto extraír o bloco "para esquerda" e plugar no bloco "para frente" e assim por diante, já foi possível resolver.</li>
                         </ul>
+                    </li>
+                    <li>
+                        <h3 class="projects-subtitle">Lib ArduinoUnit:</h3>
+                        <ul className="projects-frontend--container">
+                            <li>Documentação oficial: <a className="-linkBoldYellowProjects" href="https://github.com/mmurdoch/arduinounit" target="_blank">Clique AQUI!</a></li>
+                            <li>Biblioteca do arduíno especializada em testes unitários (bem similar ao JUnit do Java), por meio dos Assertions.</li>
+
+                            <li className="-marginNone--inMobile"><p className="-listItem--inMobile --newThemeBWMCiano">Primeiro devemos definir os métodos de testes, passando como parâmetro qual é o tipo de Assertion que estamos realizando.</p>
+                                <p className="main-title--implementFullBlock">Exemplo de teste unitário para o cenário simples "When forward is successful then returns OK":</p>
+                                <ul className="main-implementFullBlock--container">
+                                <code className="implementFullBlock">                                                    
+                                    <code className="-tokenMethod">test</code>(ok) &#123;<br/>
+                                        <code className="-nestedInnerCode"><code className="-tokenClassEntity">Serial</code>.<code className="-tokenMethod">println</code>(<code className="-tokenString">"- WHEN forward is successful THEN returns OK"</code>);</code><br/>
+                                        <code className="-nestedInnerCode"><code className="-tokenMethod">forward</code>(); <span className="-tokenComment">// Método que vamos testar, "para frente".</span></code><br/>
+                                        <code className="-nestedInnerCode"><code className="-tokenMethod">assertEqual</code>(<code className="-tokenMethod">digitalRead</code>(<code className="-tokenKeyConstant">INT1</code>, <code className="-tokenKeyConstant">HIGH</code>));</code><br/>
+                                        <code className="-nestedInnerCode"><code className="-tokenMethod">assertEqual</code>(<code className="-tokenMethod">digitalRead</code>(<code className="-tokenKeyConstant">INT2</code>, <code className="-tokenKeyConstant">HIGH</code>));</code><br/>
+                                        <code className="-nestedInnerCode"><code className="-tokenMethod">assertEqual</code>(<code className="-tokenMethod">digitalRead</code>(<code className="-tokenKeyConstant">INT3</code>, <code className="-tokenKeyConstant">LOW</code>));</code><br/>
+                                        <code className="-nestedInnerCode"><code className="-tokenMethod">assertEqual</code>(<code className="-tokenMethod">digitalRead</code>(<code className="-tokenKeyConstant">INT4</code>, <code className="-tokenKeyConstant">LOW</code>));</code><br/>
+                                        <code className="-nestedInnerCode"><code className="-tokenClassEntity">Serial</code>.<code className="-tokenMethod">println</code>(<code className="-tokenString">"- Finalizou teste SUCCESSFUL no forward..."</code>);</code><br/>
+                                    &#125;
+                                </code>                                    
+                                </ul>
+                            </li>   
+                            <li>Após definir os métodos de testes, deve-se executar a chamada para o método estático <code className="-efeitoMarcaTexto">Test::run();</code></li>
+                            <li>Que inicia a bateria de testes para TODOS os métodos de <code className="-efeitoMarcaTexto">test(ok | bad...)&#123;...&#125;</code> definidos.</li>                        
+                        </ul>
                     </li>     
                 </ul>
             </div>
