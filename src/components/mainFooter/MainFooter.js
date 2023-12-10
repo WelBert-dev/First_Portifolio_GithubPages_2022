51439161bf5081b62464496a2fa8c065046b172e
WelBert-dev <wellison.bertelli@hotmail.com>
2023-12-10 00:02:57 -0300

    Footer Component: Add E-mail corporativo "contato@wellisonbertelli.com.br".

--- src/components/mainFooter/MainFooter.js
+++ src/components/mainFooter/MainFooter.js
@@ -5,6 +5,7 @@ import { AiFillFacebook } from 'react-icons/ai';
 import { AiFillLinkedin } from 'react-icons/ai';
 import { AiFillInstagram } from 'react-icons/ai';
 import { IoLogoWhatsapp } from 'react-icons/io';
+import { MdOutlineAttachEmail } from 'react-icons/md';
 
 import './MainFooter.css';
 
@@ -13,8 +14,16 @@ export default function MainFooter() {
     return (
         <div>
         <div className='footer-description'>
-          <p>Site desenvolvido pelo Wellison Bertelli, código completo no github! ;D</p>
+          <p>Site desenvolvido pelo Wellison Bertelli ;D</p>
           <p>Redes sociais abaixo, apenas clique no Icon <span>✓</span></p>
+          <div className='footer-copyright'>
+            <div className='footer-icon-email--container'>
+              <i className='footer-icon--email'><MdOutlineAttachEmail/></i>
+              <h3>E-mail: <a href="mailto:contato@wellisonbertelli.com.br">contato@wellisonbertelli.com.br</a></h3>
+            </div>
+         
+          </div>
+          
         </div>
         <div className='footer-icons'>
           <div className='footer-icons--click'>
@@ -28,6 +37,7 @@ export default function MainFooter() {
         <div className='footer-copyright'>
             <h3>Copyright ©2022-present.</h3>
             <h3>by: Wellison Bertelli.</h3>
+            
         </div>
       </div>
     )
